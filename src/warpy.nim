21ee38881fdb599d17853bf12f28930c61a5ff72
monofuel <monofuel@japura.net>
2025-05-09 16:58:08 -0400

    none

--- src/warpy.nim
+++ src/warpy.nim
@@ -1,5 +1,5 @@
 import
-  std/[strformat],
+  std/[strformat, options, options],
   curly, jsony
 
 # https://esi.evetech.net/ui/
@@ -13,11 +13,20 @@ type
     curlTimeout: int
 
   WarpyError* = object of CatchableError
+
   GetStatus* = ref object
-    players: int # current online player count
+    players: int          # current online player count
     serverVersion: string # Running version as a string
-    startTime: string # Server start timestamp
-    vip: bool # If the server is in VIP mode
+    startTime: string     # Server start timestamp
+    vip: bool             # If the server is in VIP mode
+  WarpyResponse*[T] = ref object
+    code*: int            # HTTP status code
+    body*: Option[T]       # response body. will be none(T) if the response code is not 200 (eg: 304 no change)
+    cacheControl*: string # caching mechanism used
+    etag*: string         # RFC7232 ETag for change detection
+    expires*: string      # RFC7231 Expires header
+    lastModified*: string # RFC7231 Last-Modified header
+
 
 const
   DefaultUserAgent = "warpy"
@@ -40,15 +49,20 @@ proc newWarpy*(
 proc get*(
   api: Warpy,
   path: string,
+  ifNoneMatch: string = "",
   # TODO character stuff
 ): Response =
-  ## make a GET request to the ESI API
+  ## make a GET request to the ESI API.
+  ## ifNoneMatch can be used to provide an ETag for change detection
   var headers: HttpHeaders
   headers["User-Agent"] = api.userAgent
   headers["Accept"] = "application/json"
+  
+  if ifNoneMatch != "":
+    headers["If-None-Match"] = ifNoneMatch
 
   let resp = api.curly.get(api.baseUrl & path, headers, api.curlTimeout)
-  if resp.code != 200:
+  if resp.code != 200 and resp.code != 304:
     raise newException(
       WarpyError,
       &"API call {path} failed: {resp.code} {resp.body}"
@@ -58,10 +72,19 @@ proc get*(
 
 proc getStatus*(
   api: Warpy,
-): GetStatus =
+  ifNoneMatch: string = ""
+): WarpyResponse[GetStatus] =
   ## get the status of the server
   
   # `datasource` is an allowed query parameter, but `tranquility` is the only valid value, and is the default
 
-  let resp = api.get("/status")
-  result = resp.body.fromJson(GetStatus)
+  let resp = api.get("/status", ifNoneMatch)
+  result = WarpyResponse[GetStatus]()
+  result.code = resp.code
+  result.cacheControl = resp.headers["Cache-Control"]
+  result.etag = resp.headers["ETag"]
+  result.expires = resp.headers["Expires"]
+  result.lastModified = resp.headers["Last-Modified"]
+
+  if resp.code == 200:
+    result.body = some(resp.body.fromJson(GetStatus))
--- tests/test_public_esi.nim
+++ tests/test_public_esi.nim
@@ -1,9 +1,21 @@
 # test Public ESI API endpoints
-import std/[unittest], warpy, jsony
+import std/[unittest, options], warpy, jsony
 
 
 suite "Public ESI API":
   test "get status":
     let api = newWarpy()
-    let status = api.getStatus()
-    echo toJson(status)
+    let resp = api.getStatus()
+    assert resp.code == 200
+    assert resp.body.isSome
+    echo toJson(resp.body.get)
+
+  test "get status with etag":
+    let api = newWarpy()
+    let resp1 = api.getStatus()
+    assert resp1.code == 200
+    assert resp1.body.isSome
+    echo resp1.etag
+    let resp2 = api.getStatus(resp1.etag)
+    assert resp2.code == 304
+    assert resp2.body.isNone
